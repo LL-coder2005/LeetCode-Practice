60a05e851121a9db53ba0bcd3a3811d7dcedc0cc
李昊泽 <674078897@qq.com>
2026-03-15 07:30:20 +0000

    修改了注释

--- SlidingWindow/3679.cpp
+++ SlidingWindow/3679.cpp
@@ -16,27 +16,36 @@ using namespace std;
 
 class Solution {
 public:
+    // 函数：计算最少丢弃次数
+    // 参数：arrivals - 到达时间数组，w - 窗口大小，m - 最大允许次数
+    // 返回：最少丢弃次数
     int minArrivalsToDiscard(vector<int>& arrivals, int w, int m) {
+        // 使用哈希表记录每个到达时间的出现次数
         unordered_map<int,int> hm;
+        // 答案：丢弃次数
         int ans = 0;
+        // 遍历每个到达时间
         for(int right=0;right<arrivals.size();right++){
-            //入
+            // 入窗口：增加当前到达时间的计数
             hm[arrivals[right]]++;
 
-            //没有判断窗口是否成型
+            // 没有判断窗口是否成型，因为窗口总是从max(1, i-w+1)开始
 
-            //更新
+            // 更新：如果当前到达时间超过m次，需要丢弃
             if(hm[arrivals[right]]>m){
+                // 丢弃次数加1
                 ans++;
-                //软删除，设为一个不影响的值，防止删除两遍
+                // 软删除：减少计数，但设为-1防止后续删除时重复减少
                 hm[arrivals[right]]--;
                 arrivals[right] = -1;
+                
             }
 
-            //出
+            // 出窗口：如果窗口左侧超出范围，减少对应计数
             if(right-w+1>=0)
             hm[arrivals[right-w+1]]--;
         }
+        // 返回总丢弃次数
         return ans;
     }
 };
